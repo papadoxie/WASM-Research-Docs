0eafa5be0705739cd0622cedd101b6676b6832b6
papadoxie <nofilqasim@gmail.com>
2022-08-01 09:45:40 +0500

    Update

--- Code/vuln_helloworld/hello.c
+++ Code/vuln_helloworld/hello.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <emscripten.h>
 
 int main(void)
 {
 	char hello[] = "Hello World\n";
-	char buf[10];
-	scanf("%s", buf); // Buffer Overflow
+    char *str = malloc(255);
+    strcpy(str, "This is a test string");
+    char buf[10];
+    scanf("%s", buf); // Buffer Overflow
 	printf("%s\n", buf);
 	printf(hello);	// Format String
 	printf("\n");
+    printf("%s\n", str); // Buffer Overflow
 	return 0;
 }
